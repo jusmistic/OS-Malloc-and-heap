325ce72a1179c5cc88735320772649b1cb402330
jusmistic <jusmistic@gmail.com>
2018-04-28 22:59:13 +0700

    add some code

---
+++ src/malloc_vs_local.c
@@ -0,0 +1,16 @@
+ #include <stdio.h>
+ #include <stdlib.h>
+ #include <string.h>
+ #define NOP __asm__("NOP;NOP;NOP");
+ int main(){
+    NOP;
+    char text_local[11];
+    NOP;
+    strcpy(text_local,"We Love OS");
+    NOP;
+    char *text_heap;
+    NOP;
+    text_heap = (char *)malloc(sizeof(char)*11);
+    NOP;
+    return 1;
+ }
