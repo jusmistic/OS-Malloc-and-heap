f31eac4e426f203a2e92e08cde91d7d32e8e67a9
Puttimate "Jusmistic" Thammasang <jusmistic@gmail.com>
2018-04-29 14:04:50 +0700

    FIX INDENT

--- src/malloc.c
+++ src/malloc.c
@@ -4,19 +4,19 @@
 #define NOP __asm__("NOP;NOP;NOP");
 
 int main(){
-	NOP;
-	char text_local[6] = "LOCAL";
-	NOP;
-	printf("text_local: %s    %p\n",text_local,text_local);
+    NOP;
+    char text_local[6] = "LOCAL";
+    NOP;
+    printf("text_local: %s    %p\n",text_local,text_local);
     NOP;
     char *text_heap;
     NOP;
     text_heap = (char *)malloc(sizeof(char)*5);
-	NOP;
+    NOP;
     strcpy(text_heap,"TEST");
-   	NOP;
+    NOP;
     printf("text_heap: %s    %p\n",text_heap,text_heap);
-	NOP;
+    NOP;
     printf("---Reallocation text_heap---\n");
     NOP;
     text_heap = (char *)realloc(text_heap,sizeof(char)*12);
