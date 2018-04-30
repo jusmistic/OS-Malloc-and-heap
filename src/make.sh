05fbbba230a697cc48ab2f9c60adc799bb03f546
jusmistic <jusmistic@gmail.com>
2018-04-30 15:10:28 +0700

    fix output of gcc

--- src/make.sh
+++ src/make.sh
@@ -1,4 +1,4 @@
 echo 0 > /proc/sys/kernel/randomize_va_space &&\
-gcc -fno-pie -fno-stack-protector -z norelro -z execstack -mpreferred-stack-boundary=2 -o malloc_vs_local malloc_vs_local.c &&\
+gcc -fno-pie -fno-stack-protector -z norelro -z execstack -mpreferred-stack-boundary=2 -o malloc_vs_local.out malloc_vs_local.c &&\
 
-gcc -fno-pie -fno-stack-protector -z norelro -z execstack  -mpreferred-stack-boundary=2 -o malloc  malloc.c
+gcc -fno-pie -fno-stack-protector -z norelro -z execstack  -mpreferred-stack-boundary=2 -o malloc.out  malloc.c
